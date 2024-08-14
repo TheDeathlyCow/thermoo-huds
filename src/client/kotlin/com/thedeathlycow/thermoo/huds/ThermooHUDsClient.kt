901e540294469f36aaa00b5a0f54d82d85dda0d3
TheDeathlyCow <thedeathlycow@gmail.com>
2024-08-13 23:12:38 -1000

    initial commit

---
+++ src/client/kotlin/com/thedeathlycow/thermoo/huds/ThermooHUDsClient.kt
@@ -0,0 +1,9 @@
+package com.thedeathlycow.thermoo.huds
+
+import net.fabricmc.api.ClientModInitializer
+
+object ThermooHUDsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
